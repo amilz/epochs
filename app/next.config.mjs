7bc7a3d69b44d7ba4e5977d4350dc5195488ea22
amilz <85324096+amilz@users.noreply.github.com>
2024-04-04 20:55:18 -0700

    fix pino-pretty

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -2,23 +2,29 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config, { isServer }) => {
-      // This condition ensures that the following code block will only be executed on the client side.
-      if (!isServer) {
-        // Provide empty implementations for Node.js modules that might cause issues when bundled for the client-side.
-        config.resolve.fallback = {
-          fs: false, // Prevent webpack from trying to bundle 'fs' module for the client-side.
-          // Add other Node.js modules here if necessary, for example:
-          // path: false,
-          // os: false,
-          // etc.
-        };
-      }
-  
-      // Always return the modified config.
-      return config;
-    },
-  };
-  
-  export default nextConfig;
-  
+  webpack: (config, { isServer }) => {
+    if (config.plugins) {
+      config.plugins.push(
+        new context.webpack.IgnorePlugin({
+          resourceRegExp: /^(lokijs|pino-pretty|encoding)$/,
+        }),
+      )
+    }
+    // This condition ensures that the following code block will only be executed on the client side.
+    if (!isServer) {
+      // Provide empty implementations for Node.js modules that might cause issues when bundled for the client-side.
+      config.resolve.fallback = {
+        fs: false, // Prevent webpack from trying to bundle 'fs' module for the client-side.
+        // Add other Node.js modules here if necessary, for example:
+        // path: false,
+        // os: false,
+        // etc.
+      };
+    }
+
+    // Always return the modified config.
+    return config;
+  },
+};
+
+export default nextConfig;
