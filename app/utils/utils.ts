6f0ac16d5cb7d238af8954b7cc2de8ed4ba516f7
amilz <85324096+amilz@users.noreply.github.com>
2024-04-21 11:58:30 -0700

    chore: some UI cleanup

--- app/utils/utils.ts
+++ app/utils/utils.ts
@@ -3,6 +3,11 @@ import { Cluster } from "@solana/web3.js";
 export const getExplorerUrl = (signature: string, cluster: Cluster) => {
     return `https://explorer.solana.com/tx/${signature}?cluster=${cluster}`;
 }
+
+export const getEclipseUrl = (signature: string, cluster = 'testnet') => {
+    return `https://explorer.dev.eclipsenetwork.xyz/tx/${signature}?cluster=${cluster}`
+}
+
 export const shortenHash = (str: string) => {
     return `${str.slice(0, 4)}...${str.slice(-4)}`;
 }
